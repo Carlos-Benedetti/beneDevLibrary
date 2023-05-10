4510e29ea216e34066a3235d1d19b85842f98ca9
Carlos Eduardo Benedetti Lopes Junior <carlosbenedettidev@gmail.com>
2023-05-10 16:14:48 -0300

    feat: updated angular to 14.3

--- projects/ngx-typed-form-builder/src/lib/ngx-typed-form-builder.service.ts
+++ projects/ngx-typed-form-builder/src/lib/ngx-typed-form-builder.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, FormGroup, AbstractControl, AbstractControlOptions, ValidatorFn, AsyncValidatorFn, FormControl, FormArray } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, AbstractControl, AbstractControlOptions, ValidatorFn, AsyncValidatorFn, UntypedFormControl, UntypedFormArray } from '@angular/forms';
 
 type UnpackArrayType<T> = T extends (infer R)[] ? R : T;
 
@@ -10,8 +10,8 @@ type TypedControllerValue<T> = (
 );
 
 type TypedController<T> = (
-  T extends FormArray ? TypedFormArray<UnpackArrayType<T['value']>> : (
-    T extends FormGroup ? TypedFormGroup<T['value']> :
+  T extends UntypedFormArray ? TypedFormArray<UnpackArrayType<T['value']>> : (
+    T extends UntypedFormGroup ? TypedFormGroup<T['value']> :
     TypedAbstractControl<T>
   )
 );
@@ -23,36 +23,36 @@ export declare abstract class TypedAbstractControl<T> extends AbstractControl {
   abstract reset(value?: T, options?: Object): void;
 }
 
-export declare abstract class TypedFormControl<T> extends FormControl {
+export declare abstract class TypedFormControl<T> extends UntypedFormControl {
   readonly value: T
-  reset(value: Parameters<TypedAbstractControl<T>['reset']>[0], options?: Parameters<FormControl['reset']>[1]): void;
-  patchValue(value: Parameters<TypedAbstractControl<T>['patchValue']>[0], options?: Parameters<FormControl['patchValue']>[1]): void;
-  setValue(value: Parameters<TypedAbstractControl<T>['setValue']>[0], options?: Parameters<FormControl['setValue']>[1]): void
+  reset(value: Parameters<TypedAbstractControl<T>['reset']>[0], options?: Parameters<UntypedFormControl['reset']>[1]): void;
+  patchValue(value: Parameters<TypedAbstractControl<T>['patchValue']>[0], options?: Parameters<UntypedFormControl['patchValue']>[1]): void;
+  setValue(value: Parameters<TypedAbstractControl<T>['setValue']>[0], options?: Parameters<UntypedFormControl['setValue']>[1]): void
 }
 
 
-export declare abstract class TypedFormGroup<T> extends FormGroup {
+export declare abstract class TypedFormGroup<T> extends UntypedFormGroup {
   readonly value: { [P in keyof T]: TypedControllerValue<T[P]> };
   controls: { [P in keyof T]: TypedController<T[P]> };
   patchValue(value: { [P in keyof Partial<T>]: TypedAbstractControl<T[P]> }, options?: { onlySelf?: boolean; emitEvent?: boolean; }): void;
   setValue(value: { [P in keyof Partial<T>]: TypedAbstractControl<T[P]> }, options?: { onlySelf?: boolean; emitEvent?: boolean; }): void;
 }
 
-export declare abstract class TypedFormArray<T> extends FormArray {
+export declare abstract class TypedFormArray<T> extends UntypedFormArray {
   readonly value: TypedControllerValue<T>[];
   controls: TypedController<T>[];
-  reset(value: Parameters<TypedAbstractControl<T>['reset']>[0], options?: Parameters<FormControl['reset']>[1]): void;
+  reset(value: Parameters<TypedAbstractControl<T>['reset']>[0], options?: Parameters<UntypedFormControl['reset']>[1]): void;
   push(control: TypedAbstractControl<T>, options?: { emitEvent?: boolean; }): void;
   insert(index: number, control: TypedAbstractControl<T>, options?: { emitEvent?: boolean; }): void;
   setControl(index: number, control: TypedAbstractControl<T>, options?: { emitEvent?: boolean; }): void;
-  setValue(value: Parameters<TypedAbstractControl<T>['setValue']>[0][], options?: Parameters<FormControl['setValue']>[1]): void
-  patchValue(value: Parameters<TypedAbstractControl<T>['patchValue']>[0][], options?: Parameters<FormControl['patchValue']>[1]): void;
+  setValue(value: Parameters<TypedAbstractControl<T>['setValue']>[0][], options?: Parameters<UntypedFormControl['setValue']>[1]): void
+  patchValue(value: Parameters<TypedAbstractControl<T>['patchValue']>[0][], options?: Parameters<UntypedFormControl['patchValue']>[1]): void;
   getRawValue(): T[];
 }
 
 @Injectable()
 export class NgxTypedFormBuilderService {
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: UntypedFormBuilder) {
   }
   control<T>(formState: any, validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null, asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null): TypedFormControl<T> {
     return (<any>this.fb.control(formState, validatorOrOpts, asyncValidator)) as TypedFormControl<T>;
@@ -60,7 +60,7 @@ export class NgxTypedFormBuilderService {
   group<T>(controlsConfig: { [key in keyof T]: any; }, options?: AbstractControlOptions | { [key: string]: any; } | null): TypedFormGroup<T> {
     return (<any>this.fb.group(controlsConfig, options)) as TypedFormGroup<T>;
   }
-  array(controlsConfig: any[], validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null, asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null): FormArray {
+  array(controlsConfig: any[], validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null, asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null): UntypedFormArray {
     return this.fb.array(controlsConfig, validatorOrOpts, asyncValidator)
   }
 }
